42b41d2f0c8d7135eec743b67def9ad3688460c0
Luis Romero G <arturoromerog@gmail.com>
2022-11-15 22:31:14 -0500

    sesion12

---
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }  
+  `]
+})
+export class HeroeComponent implements OnInit {
+
+  heroe!: Heroe;
+
+  constructor(private activateRoute: ActivatedRoute, private HeroesService: HeroesService,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.activateRoute.params
+    .pipe(switchMap(({id})=>this.HeroesService.getHeroePorId(id)))
+    .subscribe(heroe => this.heroe = heroe);
+  }
+
+  regresar(){
+    this.router.navigate(['/heroes/listado']);
+  }
+}
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Heroe } from '../interfaces/heroes.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private baseUrl : string = environment.baseURL; 
+
+  constructor(private http:HttpClient ) { }
+
+  getHeroes(){
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroePorId(id:string):Observable<Heroe>{
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
+
+  }
+
+  getSugerencias(termino:string):Observable<Heroe[]>{
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${termino}&_limit=6`);
+
+  }
+
+
+
+}
